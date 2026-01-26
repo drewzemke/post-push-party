ce03af8e62d7bc1060999025e173a780978e2193
Drew Deleón-Zemke <drew.zemke@gmail.com>
2026-01-25 20:25:14 -0800

    feat(tui): improve header

--- src/tui/widgets/header.rs
+++ src/tui/widgets/header.rs
@@ -11,9 +11,16 @@ pub fn render_header(frame: &mut Frame, area: Rect, route: &Route) {
         .constraints([Constraint::Length(1), Constraint::Length(1)])
         .split(area);
 
-    // title
-    let title = Paragraph::new("POST-PUSH PARTY 🎉").style(Style::default().fg(Color::White));
-    frame.render_widget(title, chunks[0]);
+    // title bar -- shows app title and version
+    let title_chunks =
+        Layout::horizontal([Constraint::Fill(1), Constraint::Fill(1)]).split(chunks[0]);
+
+    let title = Paragraph::new("POST-PUSH PARTY 🎉").blue().bold();
+    let version = Paragraph::new(format!("v{}", env!("CARGO_PKG_VERSION")))
+        .alignment(Alignment::Right)
+        .dark_gray();
+    frame.render_widget(title, title_chunks[0]);
+    frame.render_widget(version, title_chunks[1]);
 
     // tabs
     let selected = route.tab_index();

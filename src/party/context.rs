4990d83195a8f1194a7faff6b6136fa7b4a1bb5f
Drew Deleón-Zemke <drew.zemke@gmail.com>
2026-02-08 14:46:39 -0800

    chore(party): add `RenderContext` struct

--- src/party/context.rs
+++ src/party/context.rs
@@ -1,1 +1,26 @@
-pub struct RenderContext {}
+use crate::{git::Push, history::PushHistory, scoring::PointsBreakdown, state::State};
+
+/// Constructed per-push and passed to each party that is
+/// to be rendered
+pub struct RenderContext<'a> {
+    pub push: &'a Push,
+    pub history: &'a PushHistory,
+    pub breakdown: &'a PointsBreakdown,
+    pub state: &'a State,
+}
+
+impl<'a> RenderContext<'a> {
+    pub fn new(
+        push: &'a Push,
+        history: &'a PushHistory,
+        breakdown: &'a PointsBreakdown,
+        state: &'a State,
+    ) -> Self {
+        Self {
+            push,
+            history,
+            breakdown,
+            state,
+        }
+    }
+}

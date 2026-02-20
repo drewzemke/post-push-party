fd1f803fbd7903923977535b3cbd7b590c07f658
Drew Deleón-Zemke <drew.zemke@gmail.com>
2026-02-19 20:33:04 -0800

    feat(tui): palette selector UI and code organization improvements

--- src/tui/widgets/palette_selector.rs
+++ src/tui/widgets/palette_selector.rs
@@ -16,11 +16,18 @@ impl<'a> PaletteSelector<'a> {
             active,
         }
     }
+
+    fn get_palette(&self, idx: usize) -> Option<&Palette> {
+        self.palettes
+            .get(idx)
+            .and_then(|name| ALL_PALETTES.iter().find(|&&p| p.name() == name))
+            .map(|v| &**v)
+    }
 }
 
 fn palette_preview(palette: &Palette, faded: bool) -> Line<'_> {
-    // try these: ▓▒░
-    let c = if faded { '▒' } else { '█' };
+    // try these: ▓▒░ ■
+    let c = if faded { '■' } else { '█' };
     let colors = palette.all_ratatui();
 
     let mut spans: Vec<Span> = Vec::new();
@@ -56,10 +63,7 @@ impl<'a> Widget for PaletteSelector<'a> {
         ])
         .split(area);
 
-        let center_palette = self
-            .palettes
-            .get(self.idx)
-            .and_then(|name| ALL_PALETTES.iter().find(|&&p| p.name() == name));
+        let center_palette = self.get_palette(self.idx);
 
         let (palette_name, palette_swatch) = if let Some(palette) = center_palette {
             (palette.name(), palette_preview(palette, false))
@@ -67,59 +71,54 @@ impl<'a> Widget for PaletteSelector<'a> {
             ("Random", "??????".into())
         };
 
-        let center_split = Layout::horizontal([
-            Constraint::Fill(1),   // name
+        let layout = Layout::horizontal([
+            Constraint::Fill(1),   // text
             Constraint::Length(1), // space
             Constraint::Length(6), // swatch
-        ])
-        .split(palette_chunks[1]);
+        ]);
 
-        Text::from(palette_swatch)
-            .alignment(Alignment::Left)
-            .render(center_split[2], buf);
+        let center_split = layout.split(palette_chunks[1]);
+
+        Text::from(palette_swatch).render(center_split[2], buf);
 
         if self.active {
+            // show name of the palette
             Text::from(palette_name)
                 .alignment(Alignment::Right)
                 .render(center_split[0], buf);
 
-            let top_palette = self
-                .palettes
-                .get((self.idx + self.palettes.len()) % (self.palettes.len() + 1))
-                .and_then(|name| ALL_PALETTES.iter().find(|&&p| p.name() == name));
-
+            // top/previous palette
+            let top_palette =
+                self.get_palette((self.idx + self.palettes.len()) % (self.palettes.len() + 1));
             let top_swatch = if let Some(palette) = top_palette {
-                palette_preview(palette, false)
+                palette_preview(palette, true)
             } else {
-                "??????".dim().into()
+                "••••••".dim().into()
             };
 
-            let btm_palette = self
-                .palettes
-                .get((self.idx + 1) % (self.palettes.len() + 1))
-                .and_then(|name| ALL_PALETTES.iter().find(|&&p| p.name() == name));
+            let top_split = layout.split(palette_chunks[0]);
 
+            Text::from(top_swatch).render(top_split[2], buf);
+
+            Text::from("▲")
+                .alignment(Alignment::Right)
+                .render(top_split[0], buf);
+
+            // bottom/next palette
+            let btm_palette = self.get_palette((self.idx + 1) % (self.palettes.len() + 1));
             let btm_swatch = if let Some(palette) = btm_palette {
-                palette_preview(palette, false)
+                palette_preview(palette, true)
             } else {
-                "??????".dim().into()
+                "••••••".dim().into()
             };
 
-            Text::from(top_swatch)
+            let btm_split = layout.split(palette_chunks[2]);
+
+            Text::from(btm_swatch).render(btm_split[2], buf);
+
+            Text::from("▼")
                 .alignment(Alignment::Right)
-                .render(palette_chunks[0], buf);
-
-            Text::from("            ▲")
-                .alignment(Alignment::Left)
-                .render(palette_chunks[0], buf);
-
-            Text::from(btm_swatch)
-                .alignment(Alignment::Right)
-                .render(palette_chunks[2], buf);
-
-            Text::from("            ▼")
-                .alignment(Alignment::Left)
-                .render(palette_chunks[2], buf);
+                .render(btm_split[0], buf);
         }
     }
 }
